1b5dabcc855d48755451afb23f36f0bd0868a1bd
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2017-03-08 18:18:20 +0100

    Add iTop specific fields to the form

--- library/iTop/ProvidedHook/Director/ImportSource.php
+++ library/iTop/ProvidedHook/Director/ImportSource.php
@@ -3,6 +3,7 @@
 namespace Icinga\Module\iTop\ProvidedHook\Director;
 
 use Icinga\Module\Director\Hook\ImportSourceHook;
+use Icinga\Module\Director\Web\Form\QuickForm;
 
 class ImportSource extends ImportSourceHook
 {
@@ -27,4 +28,34 @@ class ImportSource extends ImportSourceHook
 		// TODO: Implement listColumns() method.
 	}
 
+	/**
+	 * Add iTop specific fields to the form
+	 *
+	 * @param  QuickForm $form QuickForm that should be extended
+	 * @return QuickForm
+	 */
+	public static function addSettingsFormFields(QuickForm $form)
+	{
+		// TODO: Add iTop resource element
+
+		// TODO: Load queries from iTop
+
+		$form->addElement('select', 'query', array(
+			'label' => $form->translate('Query'),
+			'multiOptions' => array(
+				1 => 'Query 1',
+				2 => 'Query 2',
+				null => $form->translate('Custom…'),
+			),
+			'required' => false,
+		));
+
+		$form->addElement('radio', 'no_localize', array(
+			'label' => $form->translate('Localize'),
+			'multiOptions' => array($form->translate('Yes'), $form->translate('No')),
+			'required' => true,
+		));
+
+		return $form;
+	}
 }
